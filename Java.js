c6b1c465fe4d7ca7058448a9e2db2847c04e7f7c
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-01-12 07:09:26 +0330

    Part 4

--- Java.js
+++ Java.js
@@ -1,54 +1,30 @@
-// class Rectangle {
-//     constructor(width,height) {
-//         this.width=width
-//         this.height=height
-//     }
-//     render(){
-//       console.log(this.width,this.height)
-//     }
-// }
-
-//var obj=new Rectangle(10,20);
-//console.log(obj.width,obj.height)
-//obj.render()
-
-// class colorRectangle extends Rectangle{
-//     constructor(width,height,color){
-//         super(width,height)
-//         this.color=color
-//     }
-//     show(){
-//         return "<div style='width:"+this.width+"px;height:"+this.height+"px;background:"+this.color+"'></div>"
-//     }
-// }
-// var obj=new colorRectangle(200,100,"yellowgreen");
-// var obj2=new colorRectangle(300,500,"red");
-
-// document.getElementById("myDiv").innerHTML=obj.show()
-// document.getElementById("myDiv2").innerHTML=obj2.show()
-
-//-----2----Const,Let,Var
-
-// if(true){
-//     var x=2;
-//     let y=3;
-//     const z=5;
-// }
-
-// console.log("x:"+x)
-// console.log("y:"+y)
-// console.log("z:"+z)
-
-//------3----Function,Arrow function
-
-function test(x,y) {
-    console.log(x+y)
+class Rectangle {
+    constructor(width,height) {
+        this.width=width
+        this.height=height
+    }
+    // 2: Function
+    render(){
+        console.log(this.width,this.height);
+        
+    }
 }
-const test2=(x,y)=>{
-    console.log(x+y)
+// var obj = new Rectangle(20,30);
+// 1:
+// console.log(obj.height,obj.width);
+// 2:
+// obj.render()
+class coloredRectangle extends Rectangle {
+    constructor(width,height,color) {
+        super(width,height)
+        this.color=color
+    }
+    show(){
+        return "<div style='width:"+this.width+"px;height:"+this.height+"px;background:"+this.color+"'></div>"
+    }
 }
-const test3= y=>y*2
+var obj = new coloredRectangle(200,100,"green");
+var obj2 = new coloredRectangle(400,300,"red");
 
-test(10,20)
-test2(20,30)
-console.log(test3(4))
+document.getElementById("myDiv").innerHTML = obj.show()
+document.getElementById("myDiv2").innerHTML = obj2.show()
