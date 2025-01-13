9806db972648961d77d77c8aa3eff7586ce4a59b
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-01-13 07:01:02 +0330

    Let,Var,Const

---
+++ Let,Const,Var.js
@@ -0,0 +1,34 @@
+// -----Var,Let,Const
+// if (true) {
+//     var x=3;
+//     let y=4;
+//     const z=5;
+
+//     x=10;
+//     y=15;
+    //z=20;
+
+    
+//     console.log("x:"+x);
+//     console.log("y:"+y);
+//     console.log("z:"+z);
+    
+// }
+
+//-----Arrow Function
+// function test1(x,y) {
+//     console.log(x+y);
+    
+// }
+// test1(4,5)
+
+//------در اکما اسکریپت 6 از روش زیر استفاده می کنیم
+const test2 =(x,y)=>{
+    console.log(x+y);
+    
+}
+test2(3,5)
+
+const test3 =z=>z*2;
+console.log(test3(10));
+
